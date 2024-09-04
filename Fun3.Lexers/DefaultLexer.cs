e37ddd72af8c08e4fe51168a533397bf7b2a8292
Tim <81175852+wharvex@users.noreply.github.com>
2024-09-03 20:23:00 -0400

    progress on overlaps

--- Fun3.Lexers/DefaultLexer.cs
+++ Fun3.Lexers/DefaultLexer.cs
@@ -23,39 +23,71 @@ public class DefaultLexer(string[] lines) : ILexer
                 var finalIndexOnLine = line.Length - 1;
                 Console.WriteLine($"Line {i}: {line}");
                 Console.WriteLine($"Line {i} final idx: {finalIndexOnLine}\n");
-                TokenService
-                    .Registry.Select(kvp =>
+                var w = TokenService
+                    .Registry.SelectMany(kvp =>
                     {
                         var rg = new Regex(kvp.Key);
                         var matches = rg.Matches(line);
-                        return (
-                            Tokens: matches.Select(m => TokenService.CreateToken(kvp.Key, i, m)),
-                            KVP: kvp
-                        );
+                        return matches.Select(m => TokenService.CreateToken(kvp.Key, i, m));
                     })
-                    .ToList()
-                    .ForEach(tuple =>
+                    .ToList();
+                List<(IToken, IToken)> overlaps = [];
+                for (int j = 0; j < w.Count - 1; j++)
+                {
+                    var tok1 = w[j];
+                    if (tok1 is null)
+                        continue;
+                    for (int k = j + 1; k < w.Count; k++)
                     {
-                        var found = tuple.Tokens.Any(t => t is not null);
-                        var foundString = found ? "yes" : "no";
-                        var tokenVals = string.Join(
-                            ", ",
-                            tuple.Tokens.Select(t => t?.Value ?? "N/A")
-                        );
-                        var searchedToken = tuple.KVP.Value.FullName ?? "??";
-                        var tokenRanges = string.Join(
-                            "; ",
-                            tuple.Tokens.Select(
-                                (t, j) =>
-                                    $"Token number: {j}, Token start column: {t?.StartColumn ?? -1}, Token end column: {t?.EndColumn ?? -1}"
-                            )
-                        );
-                        Console.WriteLine($"Token type searched: {searchedToken}");
-                        Console.WriteLine($"Any matches? {foundString}");
-                        Console.WriteLine($"Found token vals: {tokenVals}");
-                        Console.WriteLine($"Found token ranges: {tokenRanges}");
-                        Console.WriteLine("\n");
-                    });
+                        var tok2 = w[k];
+                        if (tok2 is null)
+                            continue;
+                        if (tok1.Overlaps(tok2))
+                            overlaps.Add((tok1, tok2));
+                    }
+                }
+                var tokenValues = string.Join(", ", w.Select(t => t?.Value ?? "N/A"));
+                var tokenTypes = string.Join(", ", w.Select(t => t?.ToString() ?? "N/A"));
+                var tokenOverlaps = string.Join(
+                    "\n",
+                    overlaps.Select(tuple => tuple.Item1 + " overlaps " + tuple.Item2)
+                );
+                Console.WriteLine(tokenValues);
+                Console.WriteLine(tokenTypes);
+                Console.WriteLine(tokenOverlaps);
+                //TokenService
+                //    .Registry.Select(kvp =>
+                //    {
+                //        var rg = new Regex(kvp.Key);
+                //        var matches = rg.Matches(line);
+                //        return (
+                //            Tokens: matches.Select(m => TokenService.CreateToken(kvp.Key, i, m)),
+                //            KVP: kvp
+                //        );
+                //    })
+                //    .ToList()
+                //    .ForEach(tuple =>
+                //    {
+                //        var found = tuple.Tokens.Any(t => t is not null);
+                //        var foundString = found ? "yes" : "no";
+                //        var tokenVals = string.Join(
+                //            ", ",
+                //            tuple.Tokens.Select(t => t?.Value ?? "N/A")
+                //        );
+                //        var searchedToken = tuple.KVP.Value.FullName ?? "??";
+                //        var tokenRanges = string.Join(
+                //            "; ",
+                //            tuple.Tokens.Select(
+                //                (t, j) =>
+                //                    $"Token number: {j}, Token start column: {t?.StartColumn ?? -1}, Token end column: {t?.EndColumn ?? -1}"
+                //            )
+                //        );
+                //        Console.WriteLine($"Token type searched: {searchedToken}");
+                //        Console.WriteLine($"Any matches? {foundString}");
+                //        Console.WriteLine($"Found token vals: {tokenVals}");
+                //        Console.WriteLine($"Found token ranges: {tokenRanges}");
+                //        Console.WriteLine("\n");
+                //    });
             });
     }
 }
--- Fun3/IToken.cs
+++ Fun3/IToken.cs
@@ -6,4 +6,9 @@ public interface IToken
     int EndColumn { get; set; }
     int Line { get; set; }
     string Value { get; set; }
+    bool Surrounds(IToken other) =>
+        StartColumn <= other.StartColumn && EndColumn >= other.EndColumn;
+    bool Overlaps(IToken other) =>
+        (StartColumn >= other.StartColumn && StartColumn < other.EndColumn)
+        || (EndColumn >= other.StartColumn && EndColumn < other.EndColumn);
 }
